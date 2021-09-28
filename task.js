98e54609d8539545a2be32c7e2478f13d0398441
Mahmood83367 <mahmood83367@gmail.com>
2021-09-29 01:11:19 +0300

    add comment and  fix the sorting algorithm to work as expected

--- task.js
+++ task.js
@@ -4,8 +4,10 @@ let numbers = [5, 8, 0, 1, 9, 11, 15, 16]
 console.log('Original numbers list: ', numbers)
 
 for (let i = 0; i < numbers.length; i++) {
-  for (let j = 0; j < numbers.length - 1; j++) {
-    if (numbers[i] > numbers[j + 1]) {
+  //remove -1 from numbers.length Because var j start with 0
+  for (let j = 0; j < numbers.length; j++) {
+    //we cannot use i because i used for indexing
+    if (numbers[j] > numbers[j + 1]) {
       let temp = numbers[j]
       numbers[j] = numbers[j + 1]
       numbers[j + 1] = temp
@@ -13,11 +15,11 @@ for (let i = 0; i < numbers.length; i++) {
   }
 }
 
-console.log('numbers list After sorting: ', numbers)
-
+console.log('numbers list After ASC sorting: ', numbers)
+//Use the same thing in ASC Part
 for (let i = 0; i < numbers.length; i++) {
-  for (let j = 0; j < numbers.length - 1; j++) {
-    if (numbers[i] < numbers[j + 1]) {
+  for (let j = 0; j < numbers.length; j++) {
+    if (numbers[j] < numbers[j + 1]) {
       let temp = numbers[j]
       numbers[j] = numbers[j + 1]
       numbers[j + 1] = temp
